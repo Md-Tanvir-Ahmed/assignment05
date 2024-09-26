833fdda91b8b95c6bd5b82e141e8c85fb615bc27
Mohammad Tanvir Ahmed Abir <abir64072@gmail.com>
2024-09-26 13:06:06 +0600

    added first part

---
+++ js/home.js
@@ -0,0 +1,8 @@
+// Noakhali flood
+
+function noakhali(){
+    
+    const noakhaliDonation = donation('noakhali-donation');
+    console.log(noakhaliDonation)
+
+}
---
+++ js/utilities.js
@@ -0,0 +1,8 @@
+
+
+function donation(id){
+    const donatedAmount = document.getElementById(id).value;
+    const donatedNumber = parseFloat(donatedAmount);
+
+    return donatedNumber;
+}
