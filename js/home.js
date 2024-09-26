40304711ab6df8011e2401d3654fe510fcbcfc14
Mohammad Tanvir Ahmed Abir <abir64072@gmail.com>
2024-09-26 13:52:16 +0600

    added home page part

--- js/home.js
+++ js/home.js
@@ -3,6 +3,96 @@
 function noakhali(){
     
     const noakhaliDonation = donation('noakhali-donation');
-    console.log(noakhaliDonation)
+    if(!isNaN(noakhaliDonation)){
+        const noakhaliFund = document.getElementById('noakhali-fund').innerText;
+    const noakhaliFundNumber = parseFloat(noakhaliFund);
+
+     const myMoney = document.getElementById('my-money').innerText;
+     const myMoneyNumber = parseFloat(myMoney);
+
+     if(myMoneyNumber >= noakhaliDonation){
+        
+    const totalFund = noakhaliDonation + noakhaliFundNumber;
+    const newBalance = myMoneyNumber - noakhaliDonation;
+
+    document.getElementById('noakhali-fund').innerText = totalFund;
+    document.getElementById('my-money').innerText = newBalance;
+    alert('You successfully donated '+ noakhaliDonation + " BDT.");
+
+     }else{
+        alert("You don't have enough money.")
+        
+     }
+
+
+    }else{
+        alert('Please input number')
+    }
+    
+    
+
+}
+function feni(){
+    
+    const feniDonation = donation('feni-donation');
+    if(!isNaN(feniDonation)){
+        const feniFund = document.getElementById('feni-fund').innerText;
+    const feniFundNumber = parseFloat(feniFund);
+
+     const myMoney = document.getElementById('my-money').innerText;
+     const myMoneyNumber = parseFloat(myMoney);
+
+     if(myMoneyNumber >= feniDonation){
+        
+    const totalFund = feniDonation + feniFundNumber;
+    const newBalance = myMoneyNumber - feniDonation;
+
+    document.getElementById('feni-fund').innerText = totalFund;
+    document.getElementById('my-money').innerText = newBalance;
+    alert('You successfully donated '+ feniDonation + " BDT.");
+
+     }else{
+        alert("You don't have enough money.")
+        
+     }
+
+
+    }else{
+        alert('Please input number')
+    }
+    
+    
+
+}
+function quota(){
+    
+    const quotaDonation = donation('quota-donation');
+    if(!isNaN(quotaDonation)){
+        const quotaFund = document.getElementById('quota-fund').innerText;
+    const quotaFundNumber = parseFloat(quotaFund);
+
+     const myMoney = document.getElementById('my-money').innerText;
+     const myMoneyNumber = parseFloat(myMoney);
+
+     if(myMoneyNumber >= quotaDonation){
+        
+    const totalFund = quotaDonation + quotaFundNumber;
+    const newBalance = myMoneyNumber - quotaDonation;
+
+    document.getElementById('quota-fund').innerText = totalFund;
+    document.getElementById('my-money').innerText = newBalance;
+    alert('You successfully donated '+ quotaDonation + " BDT.");
+
+     }else{
+        alert("You don't have enough money.")
+        
+     }
+
+
+    }else{
+        alert('Please input number')
+    }
+    
+    
 
 }
--- js/utilities.js
+++ js/utilities.js
@@ -1,6 +1,7 @@
 
 
 function donation(id){
+   
     const donatedAmount = document.getElementById(id).value;
     const donatedNumber = parseFloat(donatedAmount);
 
